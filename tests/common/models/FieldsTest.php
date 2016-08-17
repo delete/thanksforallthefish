07ebff4c914e3eb17d05c83b3cf9c6fd13dff734
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2016-08-16 23:23:17 -0300

    add Fields and Integerfield

---
+++ tests/common/models/FieldsTest.php
@@ -0,0 +1,95 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+include "/var/www/futura/common/models/Fields.php";
+
+class IntegerFielInstancedTest extends TestCase
+{
+    protected function setUp() 
+    {
+        // name, max_length, canBeNNull, primary_key, default
+        $this->integerField = new IntegerField("id", 11, false, true, 1);
+    }
+
+    public function testConstructor()
+    {
+        $this->assertInstanceOf( IntegerField::class, $this->integerField );
+    }
+
+    public function testAttributes()
+    {
+        $this->assertObjectHasAttribute( "name", $this->integerField );
+        $this->assertObjectHasAttribute( "max_length", $this->integerField );
+        $this->assertObjectHasAttribute( "canBeNull", $this->integerField );
+        $this->assertObjectHasAttribute( "primary_key", $this->integerField );
+        $this->assertObjectHasAttribute( "default", $this->integerField );
+        $this->assertObjectHasAttribute( "type", $this->integerField );
+    }
+
+    public function testValidate()
+    {
+        $expected = $this->integerField->validate();
+
+        $this->assertTrue($expected);
+    }
+}
+
+
+class IntegerFieldExceptionsTest extends TestCase
+{
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage IntegerField must have a name!
+     */
+    public function testNameException() 
+    {
+        // name, max_length, canBeNNull, primary_key, default
+        $integerField = new IntegerField("", 1, false, true, 1);
+        $integerField->validate(); 
+    }
+    
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage max_length must be greater or equal to one!
+     */
+    public function testMaxLengthException() 
+    {
+        // name, max_length, canBeNNull, primary_key, default
+        $integerField = new IntegerField("id", 0, false, true, 1);
+        $integerField->validate(); 
+    }
+    
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage If canBeNull is false, IntegerField must have a default value!
+     */
+    public function testCanBeNullException()
+    {
+        // name, max_length, canBeNNull, primary_key, default
+        $integerField = new IntegerField("id", 11, false, true, null);
+        $integerField->validate(); 
+    }
+    
+    
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage primary_key must be boolean (true or false)!
+     */
+    public function testPrimaryKeyException() 
+    {
+        // name, max_length, canBeNNull, primary_key, default
+        $integerField = new IntegerField("id", 1, false, null, 1);
+        $integerField->validate(); 
+    }
+
+    /**
+     * @expectedException Exception
+     * @expectedExceptionMessage IntegerField default must be an int value!
+     */
+    public function testDefaultException() 
+    {
+        // name, max_length, canBeNNull, primary_key, default
+        $integerField = new IntegerField("id", 1, false, true, "a");
+        $integerField->validate();
+    }
+}
